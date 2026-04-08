d17ae7bc87e49b8499c05b12f1f14e9c5594ea34
Levon475 <buniatyan2000@gmail.com>
2026-04-08 20:18:34 +0400

    aaaaaaaaaa

---
+++ classroom/javascript/script.js
@@ -0,0 +1,12 @@
+/* let admin;
+let name = "John";
+admin = name ;
+alert ( admin );
+
+let Eart;
+let Yor_name;
+
+ */
+
+let name = prompt ("What is your name", "Levon");
+alert (` Yor name is ${name}`);
