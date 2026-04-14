c6a11605e03c5d4b52654cd7acd908b565fb98d8
Levon475 <buniatyan2000@gmail.com>
2026-04-14 20:37:18 +0400

    aaaaaaaa

--- homework/№15 — JS Authorization/script.js
+++ homework/№15 — JS Authorization/script.js
@@ -1,4 +1,4 @@
-const users = [
+/* const users = [
   { username: "admin", password: "1234", isBlocked: false },
   { username: "john", password: "qwerty", isBlocked: true },
   { username: "anna", password: "pass", isBlocked: false }
@@ -55,4 +55,39 @@ function login(x) {
 login(x)
 
 
- 
+  */
+
+
+
+const users = [
+  { username: "admin", password: "1234", isBlocked: false },
+  { username: "john", password: "qwerty", isBlocked: true },
+  { username: "anna", password: "pass", isBlocked: false }
+];
+function login(users, username, password) {
+
+  const user = users.find(u => u.username === username);
+
+
+  if (!user) {
+    console.log("Օգտատերը չի գտնվել");
+    return;
+  }
+
+
+  if (user.password !== password) {
+    console.log("Սխալ գաղտնաբառ");
+    return;
+  }
+
+  
+  if (user.isBlocked) {
+    console.log("Օգտատերը արգելափակված է");
+    return;
+  }
+
+
+  console.log(`Բարի գալուստ, ${username}`);
+}
+
+login(users,"john","qwerty")
