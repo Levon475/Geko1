893eb85ff248e8500cb586d16e1a866b815b59f8
Levon475 <buniatyan2000@gmail.com>
2026-04-25 21:29:52 +0400

    aaa

--- classroom/javascript/script.js
+++ classroom/javascript/script.js
@@ -1,51 +1,23 @@
-const balance = document.getElementById("balance")
-const quantity = document.getElementById("quantity")
-const price = document.getElementById("price")
-const buy = document.getElementById("buy")
-const sell = document.getElementById("sell")
+const ball = document.getElementById(`curs`)
+let mouseX = 0
+let mouseY = 0
+let ballX = 0
+let ballY = 0
 
-const data = {
-    price: 499,
-    quantity: 0,
-    balance: 10000,
-}
+window.addEventListener(`mousemove`, function(e) {
 
-function render(){
-    balance.textContent = data.balance
-    quantity.textContent = data.quantity
-    price.textContent = data.price
-}
+    mouseX = e.clientX
+    mouseY = e.clientY
 
-function updatePrice(){
-    const timer = setInterval(() => {
-        const random = Math.round(Math.random() * data.price * 0.05 - data.price * 0.04 / 2)
-        data.price += random
-        if(random > 0){
-            price.style.color = "#0f0"
-        }else if(random < 0){
-            price.style.color = "#f00"
-        }else{
-            price.style.color = "#fff"
-        }
-        render()
-    }, 2000);
-}
 
-buy.addEventListener("click", () => {
-    if(data.balance >= data.price){
-        data.balance -= data.price
-        data.quantity++
-    }
-    render()
-})
-sell.addEventListener("click", () => {
-    if(data.quantity > 0){
-        data.balance += data.price
-        data.quantity--
-    }
-    render()
 })
 
+function animate() {
+    ballX +=  (mouseX - ballX) * 0.1
+    ballY +=  (mouseY - ballY) * 0.1
 
-updatePrice()
-render()
+    ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+    requestAnimationFrame(animate)
+}
+
+requestAnimationFrame(animate)
